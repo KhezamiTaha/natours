39b955e4a246240c8e9abc9077f3bb58a50c59fa
KhezamiTaha <khezamitaha10@gmail.com>
2026-09-16 20:43:58 +0100

    feat: register API routes

---
+++ routes/tourRoutes.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const tourController = require('../controllers/tourController');
+
+const router = express.Router();
+
+// router.param('id', tourController.checkID);
+router.param('id', (req, res, next, value) => {
+   console.log(`Tour id is ${value}`);
+   next();
+});
+
+router.route('/tours-statistics').get(tourController.toursStatistics);
+router.route('/plan-monthly/:year').get(tourController.getPlanMonthly);
+
+router
+   .route('/trending-tours')
+   .get(tourController.getTrendingTours, tourController.getAllTours);
+
+router
+   .route('/')
+   .get(tourController.getAllTours)
+   .post(tourController.checkBody, tourController.createTour);
+
+router
+   .route('/:id')
+   .get(tourController.getOneTour)
+   .patch(tourController.updateTour);
+
+module.exports = router;
