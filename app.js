ed97ed9dd692a3497f60e193a05deb96761873ca
KhezamiTaha <khezamitaha10@gmail.com>
2026-09-17 16:27:32 +0100

    fix: improve error handling for non-existent routes

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ if (process.env.NODE_ENV == 'dev') {
 }
 app.use(express.json());
 
-// middleware for static files 
+// middleware for static files
 
 app.use(express.static(`${__dirname}/public`));
 
@@ -30,12 +30,26 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
-app.all('*', (req , res, next) => {
-   res.status(404).json({
-      status: "failed",
-      message: `There nothing on thi url : ${req.url}`
-   })
-   next()
-})
+app.all('*', (req, res, next) => {
+   // res.status(404).json({
+   //    status: 'failed',
+   //    message: `There nothing on thi url : ${req.url}`,
+   // });
+
+   const err = new Error(`There nothing on thi url : ${req.url}`);
+   err.statusCode = 404;
+   err.status=  'failed';
+   next(err);
+});
+
+app.use((err, req, res, next) => {
+   err.statusCode = err.statusCode || 500;
+   err.status = err.status || 'error';
+   res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+   });
+   next();
+});
 
 module.exports = app;
