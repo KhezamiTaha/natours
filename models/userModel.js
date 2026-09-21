72811ea0d1e372af67ab0fd50e7b5b9073f2b52e
KhezamiTaha <khezamitaha10@gmail.com>
2026-09-21 22:42:57 +0100

    feat: add password reset fields

--- models/userModel.js
+++ models/userModel.js
@@ -15,6 +15,11 @@ const userSchema = new mongoose.Schema({
       lowercase: true,
       validate: [validator.isEmail, 'Please provide a valid email'],
    },
+   role: {
+      type: String,
+      enum: ['user', 'guide', 'lead-guide', 'admin'],
+      default: 'user',
+   },
    photo: {
       type: String,
       default: 'default.jpg',
@@ -36,6 +41,14 @@ const userSchema = new mongoose.Schema({
       },
    },
    passwordChangedAt: Date,
+   passwordResetToken: {
+      type: String,
+      select: false,
+   },
+   passwordResetExpires: {
+      type: Date,
+      select: false,
+   },
 });
 
 userSchema.pre('save', function (next) {
