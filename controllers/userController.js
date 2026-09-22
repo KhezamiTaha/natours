4d7f836c2a647c5fe760b75b7223db97be50b705
KhezamiTaha <khezamitaha10@gmail.com>
2026-09-22 17:44:47 +0100

    feat: add account update and soft delete flows

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,10 @@
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
 
 /// Users function handling routes
 exports.getAllUsers = catchAsync(async (req, res) => {
-   const users = await User.find();
+   const users = await User.find({ active: { $ne: false } });
 
    res.status(200).json({
       status: 'success',
@@ -13,6 +14,60 @@ exports.getAllUsers = catchAsync(async (req, res) => {
       },
    });
 });
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+   const allowedFields = ['name', 'email'];
+   const requestedFields = Object.keys(req.body);
+   const invalidFields = requestedFields.filter(
+      (field) => !allowedFields.includes(field),
+   );
+
+   if (invalidFields.length > 0) {
+      return next(
+         new AppError(
+            `You can only update your name and email. Invalid fields: ${invalidFields.join(', ')}`,
+            400,
+         ),
+      );
+   }
+
+   if (requestedFields.length === 0) {
+      return next(
+         new AppError(
+            'Please provide a name or email to update.',
+            400,
+         ),
+      );
+   }
+
+   const updatedUser = await User.findByIdAndUpdate(
+      req.user._id,
+      req.body,
+      {
+         new: true,
+         runValidators: true,
+      },
+   );
+   const userResponse = updatedUser.toObject();
+   delete userResponse.__v;
+
+   res.status(200).json({
+      status: 'success',
+      data: {
+         user: userResponse,
+      },
+   });
+});
+
+exports.deleteMe = catchAsync(async (req, res) => {
+   await User.findByIdAndUpdate(req.user._id, { active: false });
+
+   res.status(204).json({
+      status: 'success',
+      data: null,
+   });
+});
+
 exports.createUser = (req, res) => {
    res.status(500).json({
       status: 'error',
