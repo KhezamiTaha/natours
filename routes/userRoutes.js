1f60fdafa6bc937175aad1e344ed97d998684f84
KhezamiTaha <khezamitaha10@gmail.com>
2026-09-22 17:44:31 +0100

    feat: protect auth routes with limiter

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,13 +1,37 @@
 const express = require('express');
 const userController = require('../controllers/userController');
 const authController = require('../controllers/authController');
+const { authLimiter } = require('../utils/rateLimiter');
 
 const router = express.Router();
 
-router.post('/signup', authController.signup);
-router.post('/login', authController.login);
-router.post('/forgotPassword', authController.forgotPassword);
-router.patch('/resetPassword/:token', authController.resetPassword);
+router.post('/signup', authLimiter, authController.signup);
+router.post('/login', authLimiter, authController.login);
+router.patch(
+   '/updatePassword',
+   authController.protect,
+   authController.updatePassword,
+);
+router.patch(
+   '/updateMe',
+   authController.protect,
+   userController.updateMe,
+);
+router.delete(
+   '/deleteMe',
+   authController.protect,
+   userController.deleteMe,
+);
+router.post(
+   '/forgotPassword',
+   authLimiter,
+   authController.forgotPassword,
+);
+router.patch(
+   '/resetPassword/:token',
+   authLimiter,
+   authController.resetPassword,
+);
 
 router
    .route('/')
